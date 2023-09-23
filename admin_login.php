2ae44c319bd82c0acf373fcb03eeac267adf1dbd
caraka15 <carakawidi07@gmail.com>
2023-09-23 19:52:45 +0700

    update

--- admin_login.php
+++ admin_login.php
@@ -1,61 +1,55 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user'])) {
-    header('Location: admin.php');
-    exit();
-}
-
-$db = mysqli_connect("localhost", "root", "caraka1717", "video");
-
-if (!$db) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['link'])) {
-    $link = mysqli_real_escape_string($db, $_POST['link']);
-
-    // Mengirim permintaan ke API Doodapi untuk melakukan remote upload
-    $apiKey = "238952a5hhtdv0iazrlcf3";
-    $apiUrl = "https://doodapi.com/api/upload/url?key={$apiKey}&url=" . urlencode($link);
-    $response = file_get_contents($apiUrl);
-    $data = json_decode($response, true);
-
-    // Memeriksa respon dari API Doodapi
-    if ($data && isset($data['result']['download_url'])) {
-        $downloadUrl = $data['result']['download_url'];
-
-        // Memasukkan link baru ke dalam database
-        $sql = "INSERT INTO link (link) VALUES ('$downloadUrl')";
-        if (mysqli_query($db, $sql)) {
-            echo "Link berhasil ditambahkan ke database.";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($db);
-        }
-    } else {
-        echo "Proses remote upload gagal.";
-    }
-}
-
-mysqli_close($db);
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Admin Login</title>
+    <title>Upload Video</title>
 </head>
 
 <body>
-    <h1>Admin Login</h1>
+    <h1>Upload Video</h1>
 
-    <form method="POST" action="admin_login.php">
+    <form method="POST">
         <input type="text" name="link" placeholder="Masukkan Link Video" required>
-        <button type="submit">Reupload</button>
+        <button type="submit">Upload</button>
     </form>
 
-    <a href="edit.php">Edit Video</a>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['link'])) {
+        $link = $_POST['link'];
+
+        // Ganti dengan API key Anda
+        $apiKey = "your_api_key";
+
+        // Memastikan bahwa link dimulai dengan "https://"
+        if (!preg_match("~^(?:f|ht)tps?://~i", $link)) {
+            $link = "https://" . $link;
+        }
+
+        // Buat URL API
+        $apiUrl = "https://doodapi.com/api/upload/url?key={$apiKey}&url=" . urlencode($link);
+
+        // Inisialisasi cURL session
+        $ch = curl_init($apiUrl);
+
+        // Set opsi cURL
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Eksekusi permintaan cURL
+        $response = curl_exec($ch);
+
+        // Tutup sesi cURL
+        curl_close($ch);
+
+        // Memeriksa respon dari API Doodapi
+        $data = json_decode($response, true);
+
+        if ($data && isset($data['status']) && $data['status'] === 200) {
+            echo "Status: 200 (Sukses)";
+        } else {
+            echo "Proses upload gagal.";
+        }
+    }
+    ?>
 </body>
 
 </html>
